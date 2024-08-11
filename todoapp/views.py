6dd8e809b70aa092cdc4680b08517edeb3b7cce6
“m-tashima” <tashikazu1990@gmail.com>
2024-08-11 11:56:03 +0900

    削除機能追加

--- todoapp/views.py
+++ todoapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from todoapp.models import Task
 
@@ -27,3 +27,9 @@ class TaskUpdate(UpdateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy("tasks")
+    
+class TaskDelete(DeleteView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
+    context_object_name = "task"
