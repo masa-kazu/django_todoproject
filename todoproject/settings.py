496f871b57eacdfa82299149f6354a2120d06297
“m-tashima” <tashikazu1990@gmail.com>
2024-08-12 17:18:40 +0900

    CSS設定タスク

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -119,6 +119,7 @@ LOGIN_URL = "login"
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATCFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
